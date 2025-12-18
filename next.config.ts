5b5c0ffad4f8369dc3f6083bb7fc47388ae1ddb1
kimgoid8108 <kimgoid1808@gmail.com>
2025-12-19 03:53:18 +0900

    12/19

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,6 @@ export default withPWA({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swcMinify: true,
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
